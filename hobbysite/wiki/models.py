1e70c13dc002c22a181d575397b4a7a718cc7f3b
tobi <tobey.gaerlan@student.ateneo.edu>
2025-03-04 19:48:14 +0800

    fixed datetimefield syntax

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -19,8 +19,8 @@ class ArticleCategory(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length=255)
     entry = models.TextField()
-    created_on = models.DateTimeField(False, True, editable=False)
-    updated_on = models.DateTimeField(True, True, editable=False)
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_on = models.DateTimeField(auto_now=True, editable=False)
     category_type = models.ForeignKey(
         ArticleCategory,
         null=True,
