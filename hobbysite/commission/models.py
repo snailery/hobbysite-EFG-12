62d837f36073f643c6afec6b642a9a2fd12506a1
jcpno <102474586+jcpno@users.noreply.github.com>
2025-03-05 15:12:35 +0800

    Create commission model

--- hobbysite/commission/models.py
+++ hobbysite/commission/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.PositiveIntegerField()
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_on = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        # TODO
+        return
+
+    def get_absolute_url(self):
+        # TODO
+        return
+
+    class Meta:
+        ordering = ['created_on']
