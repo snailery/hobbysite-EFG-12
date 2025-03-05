a5e4b05122c9663da1d27c0c62a96ded1c372641
jcpno <102474586+jcpno@users.noreply.github.com>
2025-03-05 15:23:34 +0800

    Create comment model

--- hobbysite/commission/models.py
+++ hobbysite/commission/models.py
@@ -19,3 +19,21 @@ class Commission(models.Model):
 
     class Meta:
         ordering = ['created_on']
+
+
+class Comment(models.Model):
+    commission = models.ForeignKey(Commission, on_delete=models.CASCADE)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_on = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        # TODO
+        return
+
+    def get_absolute_url(self):
+        # TODO
+        return
+
+    class Meta:
+        ordering = ['-created_on']
