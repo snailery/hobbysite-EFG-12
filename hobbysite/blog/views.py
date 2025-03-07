19a0a1ccef4ab86c234dd1a2c50e2ade85acc33e
jcpno <102474586+jcpno@users.noreply.github.com>
2025-03-07 23:08:03 +0800

    Adhere to PEP8 for blog app

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from blog.models import Article
 
+
 def article_list(request):
-    articles = Article.objects.all() 
+    articles = Article.objects.all()
     return render(request, 'blog/blog_index.html', {'articles': articles})
+
 
 def article_detail(request, article_id):
     article = get_object_or_404(Article, id=article_id)
