ad24a9956623f5be505514c60acfe086646456e0
acegitprac <angelonoscal13@gmail.com>
2025-03-07 21:38:16 +0800

    Created the templates and made edits on the other python files

---
+++ hobbysite/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('articles/', views.article_list, name='article_list'),
+    path('article/<int:article_id>/', views.article_detail, name='article_detail'),
+]
