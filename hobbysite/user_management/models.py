bddc5bbcbcc65cc25f198927498984d5797b85da
snailery <87980202+snailery@users.noreply.github.com>
2025-04-20 22:23:32 +0800

    created user_management app

---
+++ hobbysite/user_management/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=63)
+    email = models.EmailField()
