8fae5d63d92c6f716f26612b932cd1b1b4616e76
snailery <87980202+snailery@users.noreply.github.com>
2025-05-04 01:19:21 +0800

    added index

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import index
 
 urlpatterns = [
+    path("", index, name="dashboard"),
     path("merchstore/", include("merchstore.urls", namespace='merchstore')),
     path("wiki/", include("wiki.urls", namespace='wiki')),
     path("blog/", include("blog.urls", namespace='blog')),
