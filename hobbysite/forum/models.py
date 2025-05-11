6835e6085a61f2c2a482de2a995a0b68b7900d6c
djpredigua <djpredigua04@gmail.com>
2025-05-11 10:24:47 +0800

    Update models.py
    Modified model/s: threadcategory (former postcategory), thread (former post)
    Added model/s: comment

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.urls import reverse
 
 
-class PostCategory(models.Model):
+class ThreadCategory(models.Model):
     name = models.CharField(max_length=255)
-    desc = models.TextField()
+    description = models.TextField()
 
     def __str__(self):
         return self.name
@@ -16,9 +16,26 @@ class PostCategory(models.Model):
         ordering = ['name']
 
 
-class Post(models.Model):
+class Thread(models.Model):
     title = models.CharField(max_length=255)
+    author = models.Foreignkey(
+        Profile,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='threads'
+    )
+    category = models.Foreignkey(
+        ThreadCategory,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='threads'
+    )
     entry = models.TextField()
+    image = models.ImageField(
+        upload_to='thread_images/',
+        blank=True,
+        null=True
+    )
     created_on = models.DateTimeField(auto_now_add=True, editable=False)
     updated_on = models.DateTimeField(auto_now=True, editable=False)
     post_category = models.ForeignKey(
@@ -28,10 +45,34 @@ class Post(models.Model):
     )
 
     def __str__(self):
-        return f"[{self.post_category.name}] {self.title}: Created on:{self.created_on} Last updated on:{self.updated_on} - {self.entry}"
+        return f"[{self.thread_category.name}] {self.title}: Created on:{self.created_on} Last updated on:{self.updated_on} - {self.entry}"
 
     def get_absolute_url(self):
         return reverse('forum:thread', args=[str(self.pk)])
+
+    class Meta:
+        ordering = ['-created_on']
+
+
+class Comment(models.Model):
+    author = models.Foreignkey(
+        Profile,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='forum_comments'
+    )
+    thread = models.Foreignkey(
+        ThreadCategory,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='forum_comments'
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_on = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.entry[]
 
     class Meta:
         ordering = ['-created_on']
