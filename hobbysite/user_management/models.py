4eb7765ee6648d4afd59a5ed9b79aa28d5ad70f6
snailery <87980202+snailery@users.noreply.github.com>
2025-05-13 00:23:19 +0800

    Merge minor changes from adara/user management to dev (#13)
    * changed username to display_name

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -4,5 +4,4 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=63)
-    email = models.EmailField()
+    display_name = models.CharField(max_length=63)
--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 def passport(request, username):
+    user = User.objects.get(username=username)
     ctx = {
-        'user': User.objects.get(username=username),
+        'user': user,
+        'profile': user.profile
     }
     return render(request, 'user_management/passport.html', ctx)
