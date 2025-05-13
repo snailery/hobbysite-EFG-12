109f8aa06e065e99bc27a6c4cc2977c23c6ed9e1
jcpno <102474586+jcpno@users.noreply.github.com>
2025-05-13 15:24:51 +0800

    Update views.py

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -1,6 +1,5 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
 from .models import Commission, Job, JobApplication
 from .forms import CommissionForm, JobFormSet, CreateJobApplicationForm
 from django.shortcuts import render, redirect, get_object_or_404
@@ -26,9 +25,9 @@ def commission_create(request):
             jobs_formset.instance = commission
             jobs = jobs_formset.save()
             return redirect('commission', pk=commission.pk)  # TODO pk, but might be id
-        else:
-            commission_form = CommissionForm()
-            jobs_formset = JobFormSet()
+    else:
+        commission_form = CommissionForm()
+        jobs_formset = JobFormSet()
 
     ctx = {
         "commission": commission,
@@ -41,37 +40,18 @@ def commission_create(request):
 
 def commission_update(request, pk):
     commission = get_object_or_404(Commission, pk=pk)
-    commission_form = CommissionForm(instance=commission)
-    jobs_formset = JobFormSet(instance=commission)
-
     if (request.method == "POST"):
-        jobs_formset = JobFormSet(request.POST, instance=commission)
-        if jobs_formset.is_valid():
-            if jobs_formset.cleaned_data:
-                jobs = jobs_formset.save()
-                return redirect('commission-edit', pk=commission.pk)
-        else:
-            commission_form = CommissionForm(instance=commission)
-            ctx = {
-                "commission": commission,
-                "jobs": jobs,
-                "commission_form": commission_form,
-                "jobs_formset": jobs_formset
-            }
-            return render(request, "commission_update.html", ctx)
+        commission_form = CommissionForm(request, instance=commission)
+        jobs_formset = JobFormSet(request, instance=commission)
 
-        commission_form = CommissionForm(request.POST, instance=commission)
-        if commission_form.is_valid():
+        if commission_form.is_valid() and jobs_formset.is_valid():
             commission = commission_form.save()
-        else:
-            jobs_formset = JobFormSet(request.POST)
-            ctx = {
-                "commission": commission,
-                "jobs": jobs,
-                "commission_form": commission_form,
-                "jobs_formset": jobs_formset
-            }
-            return render(request, "commission_update.html", ctx)
+            jobs_formset.instance = commission
+            jobs = jobs_formset.save()
+            return redirect('commission', pk=commission.pk)  # TODO pk, but might be id
+    else:
+        commission_form = CommissionForm(instance=commission)
+        jobs_formset = JobFormSet(instance=commission)
 
     ctx = {
         "commission": commission,
