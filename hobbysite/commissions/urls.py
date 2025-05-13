809e4ab550f0884439094cd8f9a67abca2877485
jcpno <102474586+jcpno@users.noreply.github.com>
2025-05-13 15:00:12 +0800

    Edit App URLs

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CommissionListView, CommissionDetailView
+from .views import CommissionListView, CommissionDetailView, commission_create, commission_update
 
 urlpatterns = [
     path('list', CommissionListView.as_view(), name='commissions'),
-    path('detail/<int:pk>', CommissionDetailView.as_view(), name='commission')
+    path('detail/<int:pk>', CommissionDetailView.as_view(), name='commission'),
+    path('add', commission_create, name='commission-create'),
+    path('<int:pk>/edit', commission_update, name='commission-edit')
 ]
 
 app_name = "commissions"
