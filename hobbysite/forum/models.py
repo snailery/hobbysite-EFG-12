2f4e54abd64bc4a01ed646fdec504fb60a80d558
djpredigua <djpredigua04@gmail.com>
2025-05-14 22:10:55 +0800

    Update models.py

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -4,7 +4,7 @@ from user_management.models import Profile
 
 class ThreadCategory(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    desc = models.TextField()
 
     def __str__(self):
         return self.name
