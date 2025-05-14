30a84fbf1b89c315b58040e23cf3925d1f9c5712
jcpno <102474586+jcpno@users.noreply.github.com>
2025-05-15 02:19:03 +0800

    Customize JobApplication Formset Validation

--- hobbysite/commissions/forms.py
+++ hobbysite/commissions/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Commission, Job, JobApplication
-from django.forms import inlineformset_factory, modelformset_factory
-
+from django.forms import inlineformset_factory, modelformset_factory, BaseModelFormSet
+from django.core.exceptions import ValidationError
 
 class CommissionForm(forms.ModelForm):
     class Meta:
@@ -40,9 +40,31 @@ class JobApplicationForm(forms.ModelForm):
         self.fields["job"].disabled = True
 
 
+class BaseJobApplicationFormSet(BaseModelFormSet):
+    def clean(self):
+        # For each job, the no. of accepted applications should not exceed the manpower required
+        if any(self.errors):
+            return
+        num_accepted = {}  # { job : no. of accepted applications}
+        for form in self.forms:
+            if self.can_delete and self._should_delete_form(form):
+                continue
+
+            job = form.cleaned_data.get("job")
+            status = form.cleaned_data.get("status")
+
+            if status == "B":  # Accepted
+                num_accepted[job] = num_accepted.get(job, 0) + 1
+
+        for job, count in num_accepted.items():
+            if count > job.manpower_required:
+                raise ValidationError("Too many accepted applications")
+
+
 JobApplicationFormSet = modelformset_factory(
     JobApplication,
     form=JobApplicationForm,
+    formset=BaseJobApplicationFormSet,
     edit_only=True,
     extra=0
 )
