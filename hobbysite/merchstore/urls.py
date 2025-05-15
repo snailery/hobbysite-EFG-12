8b4ea7a1eaeacb24aacdaabc67b2da73d4a4942c
snailery <87980202+snailery@users.noreply.github.com>
2025-05-15 12:38:00 +0800

    added create view

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ItemDetailView, item_list
+from .views import ItemDetailView, item_list, item_create
 
 urlpatterns = [
     path("items", item_list, name="items"),
-    path("item/<int:pk>", ItemDetailView.as_view(), name="item")
+    path("item/<int:pk>", ItemDetailView.as_view(), name="item"),
+    path("item/add", item_create, name="item-create")
 ]
 
 app_name = "merchstore"
--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,8 +1,9 @@
-from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect, reverse
+from .models import Product, ProductType
 from user_management.models import Profile
+from django.contrib.auth.decorators import login_required
+from .forms import ProductForm
 
 
 # class ItemListView(ListView):
@@ -29,3 +30,38 @@ def item_list(request):
         "your_products": your_products
     }
     return render(request, 'merchstore/items.html', ctx)
+
+
+@login_required(login_url='/accounts/login/')
+def item_create(request):
+    prod_types = ProductType.objects.all()
+    status_choices = Product.status.field.choices
+
+    if request.method == "POST":
+        form = ProductForm(request.POST, initial={"status": "available"})
+
+        if form.is_valid():
+            p = Product()
+            p.name = request.POST.get("name")
+            p.owner = request.user.profile
+            p.desc = request.POST.get("desc")
+            p.price = request.POST.get("price")
+            p.stock = request.POST.get("stock")
+            p.prod_type = ProductType.objects.get(pk=request.POST.get("prod_type"))
+            p.save()
+
+            return redirect(reverse("merchstore:items"))
+        else:
+            print(form.errors.as_data())
+
+    else:
+        form = ProductForm(initial={"status": "available" })
+
+
+    ctx = {
+        "prod_types": prod_types,
+        "status_choices": status_choices,
+        "form": form
+    }
+    
+    return render(request, 'merchstore/item-create.html', ctx)
