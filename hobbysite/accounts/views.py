e8e898a5bbdb3d80fed0333a0029eefc30f140bc
jcpno <102474586+jcpno@users.noreply.github.com>
2025-05-16 01:36:42 +0800

    Implement New User Registration

--- hobbysite/accounts/views.py
+++ hobbysite/accounts/views.py
@@ -1,1 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ExtendedUserCreationForm
+from django.contrib.auth.models import User
+from profile.models import Profile
+
+
+def register(request):
+    if request.method == "POST":
+        register_form = ExtendedUserCreationForm(request.POST)
+
+        if register_form.is_valid():
+            user = register_form.save()
+            profile = Profile()
+            profile.user = user
+            profile.display_name = user.username
+            return redirect("/accounts/login")
+    else:
+        register_form = ExtendedUserCreationForm()
+    ctx = {}
+    return render(request, 'registration/register.html', ctx)
