87c6d2842b689a15c6cf6229d90c4241ce65cf98
jcpno <102474586+jcpno@users.noreply.github.com>
2025-05-15 12:30:49 +0800

    Add error handling to getting user

--- hobbysite/profile/views.py
+++ hobbysite/profile/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from .models import Profile
 
 
 def passport(request, username):
-    user = User.objects.get(username=username)
+    user = get_object_or_404(User, username=username)
     ctx = {
         'user': user,
         'profile': user.profile
