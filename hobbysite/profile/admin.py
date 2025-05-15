d7c628e087db9e76a70df71d9c6a0f0681444c17
tobi <tobey.gaerlan@student.ateneo.edu>
2025-05-15 21:33:12 +0800

    undo smol profile app change

--- hobbysite/profile/admin.py
+++ hobbysite/profile/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from profile.models import Profile
+from .models import Profile
 
 
 class ProfileInline(admin.StackedInline):
