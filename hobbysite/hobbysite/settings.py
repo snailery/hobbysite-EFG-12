15f995d2c0b6b0fddf7d3f1e71d831b29b5e622c
Adara Rei Dimatulac <adara.dimatulac@student.ateneo.edu>
2025-05-16 00:04:07 +0800

    Update settings.py

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     BASE_DIR/'static'
 ]
