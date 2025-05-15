6d0a8b2d7e85c13248e3331d3afe4a978af2496f
djpredigua <djpredigua04@gmail.com>
2025-05-15 13:19:02 +0800

    Update models.py

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -43,7 +43,7 @@ class Thread(models.Model):
         return f"[{self.thread_category.name}] {self.title}: Created on:{self.created_on} Last updated on:{self.updated_on} - {self.entry}"
 
     def get_absolute_url(self):
-        return reverse('forum:thread', args=[str(self.pk)])
+        return reverse('forum:thread_detail', args=[str(self.pk)])
 
     class Meta:
         ordering = ['-created_on']
