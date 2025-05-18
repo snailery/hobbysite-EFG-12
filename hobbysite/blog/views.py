48c7563327103024ca39f60b57a676b0c5133c9d
eys-coder <angelo.noscal@student.ateneo.edu>
2025-05-18 22:04:39 +0800

    Changes to Blog

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,95 +1,98 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.db.models import Prefetch
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Prefetch
 from .models import Article, ArticleCategory, Comment
 from .forms import ArticleForm, ArticleCategoryForm, ArticleUpdateForm, ArticleCommentForm
 
-@login_required
 def view_article_list(request):
-    profile = request.user.profile
-    own_articles = Article.objects.filter(author=profile)
-    other_articles = Article.objects.exclude(author=profile)
+    user_profile = getattr(request.user, 'profile', None)
+    own_articles = Article.objects.filter(author=user_profile) if user_profile else Article.objects.none()
 
-    grouped_categories = ArticleCategory.objects.prefetch_related(
-        Prefetch('articles', queryset=other_articles)
+    categories = ArticleCategory.objects.all().prefetch_related(
+        Prefetch(
+            'articles',
+            queryset=Article.objects.exclude(author=user_profile) if user_profile else Article.objects.all()
+        )
     )
 
     context = {
-        'own_articles': own_articles,
-        'articles': other_articles,
-        'categories': grouped_categories,
+        'articles_by_user': own_articles,
+        'categories': categories,
     }
-
-    return render(request, 'article_list.html', context)
-
+    return render(request, 'blog/article_list.html', context)
 
 def view_article_detail(request, num):
     article = get_object_or_404(Article, pk=num)
-    all_comments = Comment.objects.filter(article=article).order_by('-created_on')
-    similar_posts = Article.objects.filter(author=article.author).exclude(pk=article.pk)
+    user_profile = getattr(request.user, 'profile', None)
+    is_author = user_profile == article.author
 
-    if request.method == "POST":
-        form = ArticleCommentForm(request.POST)
-        if form.is_valid():
-            new_comment = form.save(commit=False)
+    related_articles = Article.objects.filter(author=article.author).exclude(pk=article.pk)
+
+    comments = Comment.objects.filter(article=article).order_by('created_on')
+
+    if request.method == 'POST' and request.user.is_authenticated:
+        comment_form = ArticleCommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.author = user_profile
             new_comment.article = article
-            if request.user.is_authenticated:
-                new_comment.author = request.user.profile
             new_comment.save()
-            return redirect('article_detail', num=article.pk)
+            return redirect('blog:article_detail', num=article.pk)
     else:
-        form = ArticleCommentForm()
+        comment_form = ArticleCommentForm()
 
     context = {
         'article': article,
-        'comments': all_comments,
-        'related_articles': similar_posts,
-        'comment_form': form,
+        'is_author': is_author,
+        'related_articles': related_articles,
+        'comments': comments,
+        'comment_form': comment_form,
     }
-
-    return render(request, 'article_detail.html', context)
+    return render(request, 'blog/article_detail.html', context)
 
 @login_required
 def create_article(request):
-    if request.method == "POST":
-        form = ArticleForm(request.POST, request.FILES)
-        cat_form = ArticleCategoryForm(request.POST)
-
-        if form.is_valid():
-            new_article = form.save(commit=False)
-            new_article.author = request.user.profile
-            new_article.save()
-            return redirect('article_list')
-
-        if cat_form.is_valid():
-            cat_form.save()
-            return redirect('article_list')
+    if request.method == 'POST':
+        if 'submit_article' in request.POST:
+            article_form = ArticleForm(request.POST, request.FILES)
+            category_form = ArticleCategoryForm()
+            if article_form.is_valid():
+                new_article = article_form.save(commit=False)
+                new_article.author = request.user.profile
+                new_article.save()
+                return redirect('blog:article_list')
+        elif 'submit_category' in request.POST:
+            category_form = ArticleCategoryForm(request.POST)
+            article_form = ArticleForm()
+            if category_form.is_valid():
+                category_form.save()
+                return redirect('blog:article_list')
     else:
-        form = ArticleForm()
-        cat_form = ArticleCategoryForm()
+        article_form = ArticleForm()
+        category_form = ArticleCategoryForm()
 
-    return render(request, 'article_create.html', {
-        'article_form': form,
-        'category_form': cat_form,
-    })
-
+    context = {
+        'article_form': article_form,
+        'category_form': category_form,
+    }
+    return render(request, 'blog/article_create.html', context)
 
 @login_required
 def update_article(request, num):
     article = get_object_or_404(Article, pk=num)
+    if request.user.profile != article.author:
+        return redirect('blog:article_detail', num=num)
 
-    if article.author != request.user.profile:
-        return redirect('article_list')
-
-    if request.method == "POST":
-        form = ArticleUpdateForm(request.POST, request.FILES, instance=article)
-        if form.is_valid():
-            form.save()
-            return redirect('article_detail', num=article.pk)
+    if request.method == 'POST':
+        update_form = ArticleUpdateForm(request.POST, request.FILES, instance=article)
+        if update_form.is_valid():
+            update_form.save()
+            return redirect('blog:article_detail', num=article.pk)
     else:
-        form = ArticleUpdateForm(instance=article)
+        update_form = ArticleUpdateForm(instance=article)
 
-    return render(request, 'article_update.html', {
+    context = {
+        'update_form': update_form,
         'article': article,
-        'update_form': form,
-    })
+    }
+    return render(request, 'blog/article_edit.html', context)
